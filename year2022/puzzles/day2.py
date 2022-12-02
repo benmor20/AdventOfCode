086a26814b7db0604b0b665b229acc5d46e2423d
benmor20 <benmor2020@gmail.com>
2022-12-02 01:08:41 -0500

    Day 2 one-liner

--- year2022/puzzles/day2.py
+++ year2022/puzzles/day2.py
@@ -41,3 +41,7 @@ class Day(Day2022):
             elif you == 'Z':
                 score += POINTS[LOSE[other]] + 6
         print(score)
+
+
+def one_line():
+    with open('year2022/data/day2data.txt', 'r') as file: print(*[(data := [l.strip().replace(' ', '') for l in file.readlines()]), sum({"AX": 3 + 1, "AY": 6 + 2, "AZ": 0 + 3, "BX": 0 + 1, "BY": 3 + 2, "BZ": 6 + 3, "CX": 6 + 1, "CY": 0 + 2, "CZ": 3 + 3}[d] for d in data), sum({"AX": 0 + 3, "AY": 3 + 1, "AZ": 6 + 2, "BX": 0 + 1, "BY": 3 + 2, "BZ": 6 + 3, "CX": 0 + 2, "CY": 3 + 3, "CZ": 6 + 1}[d] for d in data)][1:], sep='\n')
