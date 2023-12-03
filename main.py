f5c0db63d281ecd8d1375159444a0d4eecd5a2f5
benmor20 <benmor2020@gmail.com>
2023-12-02 23:54:26 -0500

    Update one liner structure

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from year2023.puzzles.day2 import Day, one_line
+from year2023.puzzles.day1 import Day, one_line
 
 
 if __name__ == '__main__':
     day = Day()
     day.puzzles()
-    print('\n--------------ONE LINER--------------')
-    one_line()
+    one_line_str = one_line()
+    if one_line_str is not None:
+        print('\n--------------ONE LINER--------------')
+        print(one_line_str)
