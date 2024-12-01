bdba8ea330ac242efc76275711350a71a3387fe8
Ben Morris <benmor2020@gmail.com>
2024-11-30 22:53:36 -0500

    2024 setup

---
+++ year2024/puzzles/day1.py
@@ -0,0 +1,21 @@
+from year2024.day2024 import Day2024
+
+
+class Day(Day2024):
+    @property
+    def num(self) -> int:
+        return 1
+
+    def get_data(self, example=False):
+        lines = super().get_data(example)
+        return lines
+
+    def puzzle1(self):
+        pass
+
+    def puzzle2(self):
+        pass
+
+
+def one_line():
+    pass
