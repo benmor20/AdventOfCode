9bc420b9497158b1af2ebf1f7cd48872d86767c1
Ben Morris <benmor2020@gmail.com>
2024-12-01 20:27:37 -0500

    2024 Day 1

--- year2024/puzzles/day1.py
+++ year2024/puzzles/day1.py
@@ -1,4 +1,5 @@
 from year2024.day2024 import Day2024
+from collections import Counter
 
 
 class Day(Day2024):
@@ -8,13 +9,28 @@ class Day(Day2024):
 
     def get_data(self, example=False):
         lines = super().get_data(example)
-        return lines
+        l1 = []
+        l2 = []
+        for line in lines:
+            vals = line.split()
+            l1.append(int(vals[0]))
+            l2.append(int(vals[1]))
+        return l1, l2
 
     def puzzle1(self):
-        pass
+        list1, list2 = self.get_data()
+        total = 0
+        for a, b in zip(sorted(list1), sorted(list2)):
+            total += abs(a - b)
+        print(total)
 
     def puzzle2(self):
-        pass
+        list1, list2 = self.get_data(True)
+        right_count = Counter(list2)
+        score = 0
+        for val in list1:
+            score += val * right_count[val]
+        print(score)
 
 
 def one_line():
