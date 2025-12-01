3c5e2c3659c44e7ca4736d737dfe8bf45303c413
Ben Morris <benmor2020@gmail.com>
2025-11-30 23:05:56 -0500

    Set up for AoC 2025

---
+++ year2025/day2025.py
@@ -0,0 +1,8 @@
+from utils.daybase import DayBase2
+from abc import ABC
+
+
+class Day2025(DayBase2, ABC):
+    @property
+    def year(self) -> int:
+        return 2025
---
+++ year2025/puzzles/dayex.py
@@ -0,0 +1,22 @@
+from year2025.day2025 import Day2025
+
+
+class Day(Day2025):
+    @property
+    def num(self) -> int:
+        return 1
+
+    def get_data(self, example=False):
+        lines = super().get_data(example)
+        data = []
+        return data
+
+    def puzzle1(self):
+        data = self.get_data(True)
+
+    def puzzle2(self):
+        data = self.get_data(True)
+
+
+def one_line():
+    pass
