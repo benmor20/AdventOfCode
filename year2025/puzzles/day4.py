cf944bcf877c2d340031ff67ab3b81674acb2cea
Ben Morris <benmor2020@gmail.com>
2025-12-04 23:54:35 -0500

    2025 day 4 one line

--- year2025/puzzles/day4.py
+++ year2025/puzzles/day4.py
@@ -26,7 +26,7 @@ class Day(Day2025):
         data = self.get_data()
         total = 0
         for space in data:
-            if sum((1 if space + d in data else 0) for d in DIRECTIONS) < 4:
+            if can_remove(space, data):
                 total += 1
         print(total)
 
@@ -43,4 +43,4 @@ class Day(Day2025):
 
 
 def one_line():
-    pass
+    return "\n".join([str(dirs := [1, -1, 1j, -1j, 1+1j, 1-1j, -1+1j, -1-1j]), str(papers := set(j + i * 1j for i, line in enumerate(open('year2025/data/day4data.txt', 'r').readlines()) for j, space in enumerate(line.strip()) if space == '@')), str(sum(1 if sum(1 if paper + d in papers else 0 for d in dirs) < 4 else 0 for paper in papers)), str(sum([len(rem := set(p for p in papers if sum(1 if p + d in papers else 0 for d in dirs) < 4)), len(papers := papers.difference(rem))][0] for _ in range(100)))][-2:])
